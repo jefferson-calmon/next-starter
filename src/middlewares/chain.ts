9eb5f5136a4eeb144337745386295f71f86dc43f
Jefferson Calmon <jeffjeff0197@gmail.com>
2025-04-18 11:20:26 -0300

    ✨ feat(middlewares/chain): Enhance `chain` middleware to properly handle next middleware execution and response propagation

--- src/middlewares/chain.ts
+++ src/middlewares/chain.ts
@@ -17,23 +17,29 @@ export function chain(
 	const current = functions[index];
 
 	if (current) {
+		// Chama o próximo middleware na cadeia
 		const next = chain(functions, index + 1);
 
+		// O current é uma função que recebe um middleware e retorna outro middleware
 		return async (
 			request: NextRequest,
 			event: NextFetchEvent,
 			response: NextResponse,
 		) => {
+			// Passa o próximo middleware para o current, que irá modificar ou ajustar o comportamento
 			const nextResponse = await current(next)(request, event, response);
 
+			// Se algum middleware retornar uma resposta (como NextResponse.redirect), retorna imediatamente
 			if (nextResponse instanceof NextResponse) {
 				return nextResponse;
 			}
 
+			// Caso contrário, continua a resposta normal
 			return response;
 		};
 	}
 
+	// Caso não haja mais middlewares, retorna a resposta original
 	return (
 		request: NextRequest,
 		event: NextFetchEvent,
