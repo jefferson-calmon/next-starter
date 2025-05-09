198e5f811f24156605096726a1af1b00f3a93e4e
Jefferson Calmon <jeffjeff0197@gmail.com>
2025-05-09 15:38:35 -0300

    ✨ feat(icon): Add `IconName` type for better type safety in `IconProps`

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -2,6 +2,8 @@ import { memo } from 'react';
 
 import { icons, LucideProps } from 'lucide-react';
 
+export type IconName = keyof typeof icons;
+
 interface IconProps extends LucideProps {
 	name: keyof typeof icons;
 }
