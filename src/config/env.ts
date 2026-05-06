191333136bfb8f4b4acfa88de259cb26a9502885
Jefferson Calmon <jeffjeff0197@gmail.com>
2026-05-06 17:18:07 -0300

    feat(config/env): Add `NEXT_PUBLIC_BASE_URL` to client environment variables and remove `APP_URL` from server configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,11 +1,10 @@
 import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
-const server = {
-	APP_URL: z.string(),
-};
+const server = {};
 
 const client = {
+	NEXT_PUBLIC_BASE_URL: z.string(),
 	NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION_ID: z.string(),
 };
 
@@ -14,7 +13,7 @@ export const env = createEnv({
 	client,
 
 	runtimeEnv: {
-		APP_URL: process.env.APP_URL,
+		NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
 		NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION_ID:
 			process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION_ID,
 	},
