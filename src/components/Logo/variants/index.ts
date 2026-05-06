af47caae7ce88824daf228036cad5384a6da7a4a
Jefferson Calmon <jeffjeff0197@gmail.com>
2026-05-06 16:52:47 -0300

    chore(components): Remove unused components and styles including `Button`, `Icon`, `Input`, `Link`, `LoadingFullScreen`, `Logo`, `NavigationProgressBar`, and `TextArea`

--- src/components/Logo/variants/index.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentProps, CSSProperties } from 'react';
-
-export interface VariantLogoProps extends ComponentProps<'svg'> {
-	fill: string;
-	width: CSSProperties['width'];
-	height: CSSProperties['height'];
-}
