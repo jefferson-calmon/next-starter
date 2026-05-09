0a1cc074a4e49f12be956b0e6f437189a4e79080
Jefferson Calmon <jeffjeff0197@gmail.com>
2026-05-09 05:17:47 -0300

    feat(config): Add experimental `typedEnv` support in `next.config.ts`

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+	experimental: {
+		typedEnv: true,
+	},
+
 	transpilePackages: ['lucide-react', 'geist'],
 	typedRoutes: true,
 
